ffbd021af4a3de827be4f4dcf5d80a3a8c6ca677
송이현 <songehyun00@gmail.com>
2024-06-05 11:56:54 +0900

    중간저장

---
+++ test.js
@@ -0,0 +1,12 @@
+const a = `https://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=HV8E%2BTY8D2sM0K0m4ySRar%2F5gcEogz%2FBFEg%2BWr5P0t7m67j5Mg6xuAy1wGLmzhNFVsgSQ%2BwjmzOshIxLiknILw%3D%3D&`;
+
+const b = "endpoint 주소" + "내가 받은 키에 대한 주소값";
+
+const endpoint = "http://apis.data.go.kr/6300000/eventDataService";
+
+const keyName =
+  "HV8E%2BTY8D2sM0K0m4ySRar%2F5gcEogz%2FBFEg%2BWr5P0t7m67j5Mg6xuAy1wGLmzhNFVsgSQ%2BwjmzOshIxLiknILw%3D%3D";
+
+const example = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${keyName}&numOfRows=10&pageNo=1`;
+
+console.log(example);
